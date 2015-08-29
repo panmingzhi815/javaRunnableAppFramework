18787c5acf0af79d6a091076c8b6d0ee634f5625
panmingzhi815 <154341736@qq.com>
2015-08-30 00:01:35 +0800

    添加基本的套件模型，可用maven package 后直接运行main.exe

---
+++ src/main/java/AppMain.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+/**
+ * Created by panmingzhi on 2015/8/29.
+ */
+public class AppMain {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"java 桌面应用，我来了");
+    }
+}
